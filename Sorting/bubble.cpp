679f422a3877353991c0eb8f314e19cd86ead9a3
vbevia <vbevia@gmail.com>
2024-06-18 15:47:16 +0200

    Refactored Loop with continue

--- Sorting/bubble.cpp
+++ Sorting/bubble.cpp
@@ -20,12 +20,22 @@ void bubbleSort(std::vector<int>& arr) { //Takes a reference to a std::vector<in
     bool swapped; //The method uses a boolean variable swapped to keep track of whether any elements were swapped during a pass through the array. It is initially set to false.
     for (int i = 0; i < n - 1; ++i) { //The outer loop runs n - 1 times. It iterates from i = 0 to i < n - 1. This loop controls the number of passes through the array.
         swapped = false;
+       /*
         for (int j = 0; j < n - i - 1; ++j) { //Inside the outer loop, the inner loop runs from j = 0 to j < n - i - 1. This loop compares adjacent elements in the array and swaps them if they are in the wrong order.
             if (arr[j] > arr[j + 1]) { //If arr[j] is greater than arr[j + 1], it means that the elements are out of order. 
-                std::swap(arr[j], arr[j + 1]); //In that case, the std::swap function is used to swap the elements. This function swaps the values of the two elements.
+               std::swap(arr[j], arr[j + 1]); //In that case, the std::swap function is used to swap the elements. This function swaps the values of the two elements.
                 swapped = true; //After swapping, the swapped variable is set to true to indicate that a swap has occurred.
                 //At the end of each pass through the inner loop, the swapped variable is checked. If no elements were swapped during the pass, it means that the array is already sorted, and there is no need to continue the sorting process. In that case, the break statement is used to exit the outer loop.
              //Finally, when the sorting process is complete, the array arr will be sorted in ascending order.
+            }
+        */
+        //an alternative implementation of the inner loop that avoids unnecessary comparisons when the array is already sorted.
+        for (int j = 0; j < n - i - 1; ++j) {
+            if (arr[j] <= arr[j + 1]) {
+                continue;
+            }
+            std::swap(arr[j], arr[j + 1]);
+            swapped = true;
             }
         }
         //If no two elements were swapped in the inner loop, then the array is already sorted
