2af99569a2a6881f7edb2ee9031c19c9dc2e74e7
vbevia <vbevia@gmail.com>
2024-11-15 09:24:47 +0100

    add comments to the recursive fibonacci algorithm

--- DynamicProgramming/recursive.cpp
+++ DynamicProgramming/recursive.cpp
@@ -7,6 +7,17 @@ int fibonacci(int n) {
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
+// this case has time complexity of n^2, for instance for n = 5 calls we have 4 calls and for n = 6 we have 23 calls!
+/*
+For example:
+	•	 n = 3 : 5 calls.
+	•	 n = 4 : 9 calls.
+	•	 n = 5 : 15 calls.
+	•	 n = 6 : 29 calls.
+*/
+
+// the total number of calls is roughly proportional to  2^n 
+
 int main() {
     int n = 10; // Example value
     std::cout << "Fibonacci(" << n << ") = " << fibonacci(n) << std::endl;
