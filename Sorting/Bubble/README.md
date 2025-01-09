edc197bdcb6f314bd623cf5d74a72545fb281f39
vbevia <vbevia@gmail.com>
2025-01-09 09:11:59 +0100

    clean BFS

--- Sorting/Bubble/README.md
+++ Sorting/Bubble/README.md
@@ -1,16 +1,30 @@
 ## Explanation
 
-    #include <iostream>: Includes the input-output stream library for console output.
-    #include <vector>: Includes the vector library for using std::vector.
-    bubbleSort function:
-        Takes a reference to a std::vector<int> as its parameter.
-        Iterates through the vector multiple times, comparing each pair of adjacent elements.
-        Swaps elements if they are in the wrong order.
-        If no swaps occur during an iteration, the algorithm breaks early as the array is already sorted.
-    main function:
-        Initializes a vector with unsorted integers.
-        Prints the unsorted array.
-        Calls the bubbleSort function to sort the array.
-        Prints the sorted array.
+#include <iostream>: Includes the input-output stream library for console output.
+#include <vector>: Includes the vector library for using std::vector.
+bubbleSort function:
+    Takes a reference to a std::vector<int> as its parameter.
+    Iterates through the vector multiple times, comparing each pair of adjacent elements.
+    Swaps elements if they are in the wrong order.
+    If no swaps occur during an iteration, the algorithm breaks early as the array is already sorted.
+main function:
+    Initializes a vector with unsorted integers.
+    Prints the unsorted array.
+    Calls the bubbleSort function to sort the array.
+    Prints the sorted array.
 
 This example uses modern C++ features such as std::vector and range-based for loops to make the code more readable and easier to manage.
+
+to make it run:
+g++ ./Sorting/Bubble/bubble.cpp ./Sorting/Bubble/bubble.cpp -o bubble 
+To enable C++11, use the following command:
+g++ -std=c++11 ./Sorting/Bubble/bubble.cpp -o ./Sorting/Bubble/bubble -o bubble
+To run the program, use this command:
+./bubble
+
+// Function to implement Bubble Sort
+
+The bubbleSort method is an implementation of the Bubble Sort algorithm. It takes in a reference to a std::vector<int> called arr, 
+which represents the array to be sorted. The method sorts the array in ascending order.
+as it has a time complexity of O(n^2).
+
--- Sorting/Bubble/bubble.cpp
+++ Sorting/Bubble/bubble.cpp
@@ -1,31 +1,21 @@
 #include <iostream>
 #include <vector>
 
-//  to make it run:
-//  g++ ./Sorting/Bubble/bubble.cpp ./Sorting/Bubble/bubble.cpp -o bubble 
-//  To enable C++11, use the following command:
-//  g++ -std=c++11 ./Sorting/Bubble/bubble.cpp -o ./Sorting/Bubble/bubble -o bubble
-//  To run the program, use this command:
-//  ./bubble
-
-// Function to implement Bubble Sort
-/*
-The bubbleSort method is an implementation of the Bubble Sort algorithm. It takes in a reference to a std::vector<int> called arr, 
-which represents the array to be sorted. The method sorts the array in ascending order.
-as it has a time complexity of O(n^2).
-
-*/
 void bubbleSort(std::vector<int>& arr) { //Takes a reference to a std::vector<int> as its parameter.
     int n = arr.size(); //It starts by initializing a variable n with the size of the input array arr. This will be used to control the number of iterations in the outer loop.
     bool swapped; //The method uses a boolean variable swapped to keep track of whether any elements were swapped during a pass through the array. It is initially set to false.
     for (int i = 0; i < n - 1; ++i) { //The outer loop runs n - 1 times. It iterates from i = 0 to i < n - 1. This loop controls the number of passes through the array.
         swapped = false;
        /*
-        for (int j = 0; j < n - i - 1; ++j) { //Inside the outer loop, the inner loop runs from j = 0 to j < n - i - 1. This loop compares adjacent elements in the array and swaps them if they are in the wrong order.
+        for (int j = 0; j < n - i - 1; ++j) { //Inside the outer loop, the inner loop runs from j = 0 to j < n - i - 1. 
+        This loop compares adjacent elements in the array and swaps them if they are in the wrong order.
             if (arr[j] > arr[j + 1]) { //If arr[j] is greater than arr[j + 1], it means that the elements are out of order. 
-               std::swap(arr[j], arr[j + 1]); //In that case, the std::swap function is used to swap the elements. This function swaps the values of the two elements.
+               std::swap(arr[j], arr[j + 1]); //In that case, the std::swap function is used to swap the elements. 
+               This function swaps the values of the two elements.
                 swapped = true; //After swapping, the swapped variable is set to true to indicate that a swap has occurred.
-                //At the end of each pass through the inner loop, the swapped variable is checked. If no elements were swapped during the pass, it means that the array is already sorted, and there is no need to continue the sorting process. In that case, the break statement is used to exit the outer loop.
+                //At the end of each pass through the inner loop, the swapped variable is checked. If no elements 
+                were swapped during the pass, it means that the array is already sorted, and there is no need 
+                to continue the sorting process. In that case, the break statement is used to exit the outer loop.
              //Finally, when the sorting process is complete, the array arr will be sorted in ascending order.
             }
         */
