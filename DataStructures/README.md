34ec9a392877597c5554a6070621c38894d5d0c9
vbevia <vbevia@gmail.com>
2025-02-06 09:46:27 +0100

    add mergeSortedArrays

--- DataStructures/README.md
+++ DataStructures/README.md
@@ -2,5 +2,7 @@
 g++ ./DataStructures/mergeSortedArrays.cpp ./DataStructures/mergeSortedArrays.cpp -o mergeSortedArrays 
 ### To enable C++11, use the following command:
 g++ -std=c++11 ./DataStructures/mergeSortedArrays.cpp -o ./DataStructures/mergeSortedArrays.cpp -o mergeSortedArrays 
+g++ -std=c++11 ./DataStructures/mergeSortedArraysOpt.cpp -o ./DataStructures/mergeSortedArraysOpt.cpp -o mergeSortedArraysOpt 
 ### To run the program, use this command:
 ./mergeSortedArrays
+./mergeSortedArraysOpt
---
+++ DataStructures/mergeSortedArraysOpt.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+void merge(std::vector<int>& nums1, int m, const std::vector<int>& nums2, int n) {
+    int i = m - 1;  // Last valid element in nums1
+    int j = n - 1;  // Last element in nums2
+    int k = m + n - 1; // Last position in nums1
+    
+    // Merge from the back to avoid overwriting elements in nums1
+    while (i >= 0 && j >= 0) {
+        nums1[k--] = (nums1[i] > nums2[j]) ? nums1[i--] : nums2[j--];
+    }
+    
+    // If there are remaining elements in nums2, copy them
+    while (j >= 0) {
+        nums1[k--] = nums2[j--];
+    }
+}
+
+int main() {
+    std::vector<int> nums1 = {1, 2, 3, 0, 0, 0};
+    int m = 3;
+    std::vector<int> nums2 = {2, 5, 6};
+    int n = 3;
+
+    merge(nums1, m, nums2, n);
+
+    std::cout << "Merged array: ";
+    for (int num : nums1) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
