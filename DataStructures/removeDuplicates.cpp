9c66f79d5d0e6d5f32c84e1e9238ee996c1a7379
vbevia <vbevia@gmail.com>
2025-02-06 15:29:24 +0100

    added more tests

---
+++ DataStructures/removeDuplicates.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+using namespace std;
+
+// Function implementation
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0; // Position to place valid elements
+        for (int num : nums) {
+            if (i < 2 || num != nums[i - 2]) {
+                nums[i++] = num;
+            }
+        }
+        return i;
+    }
+};
+
+// Test function
+void test() {
+    Solution solution;
+    
+    // Example test case
+    vector<int> nums = {1, 1, 1, 2, 2, 3}; // Input array
+    vector<int> expectedNums = {1, 1, 2, 2, 3}; // Expected output
+
+    int k = solution.removeDuplicates(nums); // Call function
+    nums.resize(k); // Resize nums to the new length
+    
+    // Verify length matches
+    assert(k == expectedNums.size());
+
+    // Verify elements match
+    for (int i = 0; i < k; i++) {
+        assert(nums[i] == expectedNums[i]);
+    }
+
+    cout << "Test passed successfully!" << endl;
+}
+
+int main() {
+    test();
+    return 0;
+}
